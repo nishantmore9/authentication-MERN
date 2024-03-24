38343d3652ce36e0628bfa27380e726592222301
Nishant More <nishantmore228@gmail.com>
2024-03-24 18:20:44 +0530

    Add signup controller, auth routes and user model

---
+++ server/src/controllers/auth.controllers.js
@@ -0,0 +1,19 @@
+import { User } from "../models/users.model.js"
+import bcryptjs from "bcryptjs"
+
+export const signUp = async(req, res) => {
+  const {username, email, password} = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const newUser = new User({
+    username,
+    email,
+    password : hashedPassword
+  })
+
+  try {
+    await newUser.save()  
+    return res.status(201).json({message : "user created succesfully"})  
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
-const PORT = process.env.PORT || 8000
-dotenv.config();
+app.use(express.json())
+
+dotenv.config({
+  path : ".env"
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log(`MongoDB connected`))
   .catch((error) => console.log(error));
 
+//Routes
+app.use("/api/auth", authRoutes)
 
-
-app.listen(PORT, () => console.log(`Server started at PORT : ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server started at PORT : ${process.env.PORT}`));
